f4f0df94385cd4c2c1584d602d7e4876aadb3b65
enciney <enginyldrm02@gmail.com>
2017-12-17 17:28:09 +0300

    Initial commit

--- src/com/encine/Main.java
+++ src/com/encine/Main.java
@@ -2,23 +2,13 @@ package com.encine;
 
 public class Main {
 	static final String   fileName = "src\\pyramid.txt" ; 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		Pyramid pyramid = new Pyramid();
 		pyramid.readFile(fileName);
-		
-		
 		System.out.println( pyramid.foundMaxSum());
 
 	}
 	
-	public static void readFile() {
-		
-		
-		
-	}
 
 }
